ff7ce104bf381998e48e70a38aed89c8a1a21d47
Y7T007 <yassir7t@gmail.com>
2023-12-16 20:53:25 +0100

    ex2-2

---
+++ TP2/src/yassir/wahid/exercice2/Compte.java
@@ -0,0 +1,21 @@
+package yassir.wahid.exercice2;
+
+public class Compte {
+
+		int solde=0 ;
+		void deposer(int montant){
+		solde =solde+montant ;
+		}
+		void retirer (int montant){
+		solde=solde-montant ;
+		}
+		void virerVers(int montant, Compte destination){
+		this.retirer(montant) ;
+		destination.deposer(montant) ;
+		}
+		
+		void afficher(){
+		System.out.println("solde : "+solde) ; 
+		}
+		
+}
---
+++ TP2/src/yassir/wahid/exercice2/Main.java
@@ -0,0 +1,11 @@
+package yassir.wahid.exercice2;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Compte c1 = new Compte();
+		Compte c2 = new Compte();
+		
+	}
+
+}
