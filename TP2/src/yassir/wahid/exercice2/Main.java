bf99c31da700d67bea53f6df5cb31990a718c199
Y7T007 <yassir7t@gmail.com>
2023-12-16 20:54:58 +0100

    ex2-q1-a.b

--- TP2/src/yassir/wahid/exercice2/Main.java
+++ TP2/src/yassir/wahid/exercice2/Main.java
@@ -3,8 +3,13 @@ package yassir.wahid.exercice2;
 public class Main {
 	
 	public static void main(String[] args) {
+		
 		Compte c1 = new Compte();
 		Compte c2 = new Compte();
+		
+		
+		c1.deposer(500);
+		c2.deposer(1000);
 		
 	}
 
