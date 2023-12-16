8d3596e8b6b0584f4f8ea08ffe76086363fbd9c6
Y7T007 <yassir7t@gmail.com>
2023-12-16 21:23:59 +0100

    Q6

--- TP2/src/yassir/wahid/exercice2/Compte.java
+++ TP2/src/yassir/wahid/exercice2/Compte.java
@@ -1,30 +1,32 @@
 package yassir.wahid.exercice2;
 
 public class Compte {
+  int solde = 0;
+  String Nom = "";
 
-		int solde=0 ;
-		String Nom="";
-		
-		Compte(String Nom) {
-			this.Nom=Nom;
+  Compte(String Nom) {
+    this.Nom = Nom;
+  }
+
+  Compte() {}
+
+  void deposer(int montant) {
+    solde = solde + montant;
+  }
+
+  void retirer(int montant) {
+    solde = solde - montant;
+  }
+  
+
+  void virerVers(int montant, Compte destination) {
+		if (this.solde >= montant) {
+			this.retirer(montant);
+			destination.deposer(montant);
 		}
-		Compte(){
-			
-		}
-		
-		void deposer(int montant){
-		solde =solde+montant ;
-		}
-		void retirer (int montant){
-		solde=solde-montant ;
-		}
-		void virerVers(int montant, Compte destination){
-		this.retirer(montant) ;
-		destination.deposer(montant) ;
-		}
-		
-		void afficher(){
-		System.out.println("solde : "+solde+", Nom : "+Nom) ; 
-		}
-		
+  }
+
+  void afficher() {
+    System.out.println("solde : " + solde + ", Nom : " + Nom);
+  }
 }
