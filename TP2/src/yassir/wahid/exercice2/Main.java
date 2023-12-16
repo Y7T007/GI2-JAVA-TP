99fa93826ddc438a068b1cc51a5a2a15ae1a7a9a
Y7T007 <yassir7t@gmail.com>
2023-12-16 21:01:22 +0100

    Ex2-3 done

--- TP2/src/yassir/wahid/exercice2/Main.java
+++ TP2/src/yassir/wahid/exercice2/Main.java
@@ -4,12 +4,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		Compte c1 = new Compte();
-		Compte c2 = new Compte();
-		
-		
-		c1.deposer(500);
-		c2.deposer(1000);
+
 		
 	}
 
---
+++ TP2/src/yassir/wahid/exercice2/TestCompte.java
@@ -0,0 +1,24 @@
+package yassir.wahid.exercice2;
+
+public class TestCompte {
+	
+	public static void main(String[] args) {
+		Compte c1 = new Compte();
+		Compte c2 = new Compte();
+		
+		
+		c1.deposer(500);
+		
+		c2.deposer(1000);
+		
+		c2.retirer(10);
+		
+		c1.virerVers(75, c2);
+		
+		c1.afficher();
+		c2.afficher();
+		
+		
+	}
+
+}
