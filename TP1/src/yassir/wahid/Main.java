11492aa30d5b10faf4bc5d7b4138056a905442b8
Y7T007 <yassir7t@gmail.com>
2023-12-16 19:03:45 +0100

    Q1

--- TP1/src/yassir/wahid/Main.java
+++ TP1/src/yassir/wahid/Main.java
@@ -1,10 +1,25 @@
 package yassir.wahid;
 
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("	hello world ");
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		int i,j;
+		System.out.println("entrez le premier nombre");
+		i= scanner.nextInt();
+
+		System.out.println("entrez le Deuxieme nombre");
+		j= scanner.nextInt();
+		
+		System.out.println("Somme des deux noimbres est : "+ (i+j));
+
+		System.out.println("Produit des deux noimbres est : "+ (i*j));
 	}
 	
 }
