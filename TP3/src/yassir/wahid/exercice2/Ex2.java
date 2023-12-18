5e756b896ca2197ce11cf9fa1ff6aca1ebd69c2b
Y7T007 <yassir7t@gmail.com>
2023-12-18 12:01:43 +0100

    Exercice 1 done

--- TP3/src/yassir/wahid/exercice2/Ex2.java
+++ TP3/src/yassir/wahid/exercice2/Ex2.java
@@ -5,11 +5,13 @@ public class Ex2 {
 	public static void main(String[] args) {
 		TabUtils tab = new TabUtils();
 
-		int[10] pTab1 = {1, 2, 3, 5, 4, 6, 9, 7};
-		int[10] pTab2 = {1, 8, 3, 6, 4, 6, 8, 7};
+		int[] pTab1 = {1, 2, 3, 5, 4, 6, 9, 7};
+		int[] pTab2 = {1, 8, 3, 6, 4, 6, 8, 7};
 
-		System.out.println(tab.compareTab(pTab1, pTab2));
+		int[] pTab3 = tab.compareTab(pTab1, pTab2);
 
+		for (int i = 0; i < pTab3.length; i++) {
+			System.out.println(pTab3[i]);
+		}
 	}
-
 }
