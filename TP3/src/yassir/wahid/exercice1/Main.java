48c9e006a56ccd8b89a132b772dbc408acee6a95
Y7T007 <yassir7t@gmail.com>
2023-12-18 11:16:25 +0100

    Fix compilation errors and initialize Comptes list in Main.java

--- TP3/src/yassir/wahid/exercice1/Main.java
+++ TP3/src/yassir/wahid/exercice1/Main.java
@@ -1,7 +1,14 @@
 package yassir.wahid.exercice1;
 
+import java.util.ArrayList;
+
 public class Main {
 	
-	ArrayList<Compte> main = new ArrayList<>;
-
+	static ArrayList<Compte> Comptes = new ArrayList<>();
+	
+	static {
+		Comptes.add(new Compte(1, 1000, "John Doe"));
+		Comptes.add(new Compte(2, 2000, "Jane Smith"));
+		Comptes.add(new Compte(3, 3000, "Alice Johnson"));
+	}
 }
