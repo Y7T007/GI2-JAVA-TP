1f97d0eb6aaebdb99a94729f04769ed94502dedc
Y7T007 <yassir7t@gmail.com>
2023-12-18 12:36:05 +0100

    Exercice 5 done

--- TP3/src/yassir/wahid/exercice5/Exercice5.java
+++ TP3/src/yassir/wahid/exercice5/Exercice5.java
@@ -1,5 +1,17 @@
 package yassir.wahid.exercice5;
 
-public class Exercice5 {
+import java.util.Scanner;
 
+public class Exercice5 {
+	public static void main(String[] args) {
+		System.out.println("Saisissez la phrase : \n");
+		Scanner scanner = new Scanner(System.in);
+		String Phrase = scanner.nextLine();
+
+		String[] words = Phrase.split(" ");
+
+
+
+		System.out.println("le nombre de Mots est : " + words.length);
+	}
 }
