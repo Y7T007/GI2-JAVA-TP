94b4347b9dba669beeacfaeceba54d8d0d6db0eb
Y7T007 <yassir7t@gmail.com>
2023-12-18 12:51:22 +0100

    Add login and password setters in Utilisateur class

--- TP4/src/yassir/wahid/exercice1/Utilisateur.java
+++ TP4/src/yassir/wahid/exercice1/Utilisateur.java
@@ -26,4 +26,27 @@ public class Utilisateur extends Personne {
 	public String getPassword() {
 		return Password;
 	}
+
+	@Override
+	public void setId(int id) {
+		// TODO Auto-generated method stub
+		super.setId(id);
+	}
+	@Override
+	public void setNom(String nom) {
+		// TODO Auto-generated method stub
+		super.setNom(nom);
+	}
+	@Override
+	public void setSalaire(int salaire) {
+		// TODO Auto-generated method stub
+		super.setSalaire(salaire);
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public void setPassword(String password) {
+		Password = password;
+	}
+
 }
