ff19723ed8b1ad3e72b77e859970d9d4aca3384c
Y7T007 <yassir7t@gmail.com>
2023-12-18 13:34:10 +0100

    Update Eclipse JDT Core index file

---
+++ TP4/src/yassir/wahid/exercice2/Main.java
@@ -0,0 +1,15 @@
+package yassir.wahid.exercice2;
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		CD cd1 = new CD(1, "cd 1", "yassir", 15.75f);
+		Livre l1 = new Livre(1, "WAHID", "123456798", "Livre 1");
+
+		cd1.Print();
+		l1.Print();
+	}
+	
+
+}
