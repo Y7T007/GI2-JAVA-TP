70c8a6a67a2220b528e9b24f40710185039749c4
Y7T007 <yassir7t@gmail.com>
2023-12-18 12:45:45 +0100

    Add id, nom, and Salaire properties to Personne class

--- TP4/src/yassir/wahid/exercice1/Personne.java
+++ TP4/src/yassir/wahid/exercice1/Personne.java
@@ -1,5 +1,49 @@
 package yassir.wahid.exercice1;
 
 public class Personne {
+	int id;
+	String nom;
+	int Salaire;
+
+	public int getId() {
+		return id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public int getSalaire() {
+		return Salaire;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public void setSalaire(int salaire) {
+		Salaire = salaire;
+	}
+
+	// constructeur par default
+
+	public Personne() {
+	}
+
+	// constructeur par initialization
+
+	public Personne(int id, String nom, int Salaire) {
+		this.id = id;
+		this.nom = nom;
+		this.Salaire = Salaire;
+	}
+
+	// constructeur par copie
+
+	public Personne(Personne P) {
+		this.id = P.id;
+		this.nom = P.nom;
+		this.Salaire = P.Salaire;
+	}
 
 }
