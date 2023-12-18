6557a1fef75900a2c49a8a23fef8532ef97a62f5
Y7T007 <yassir7t@gmail.com>
2023-12-18 13:16:45 +0100

    Add idProduit field and getter/setter methods to Produit class

--- TP4/src/yassir/wahid/exercice2/Produit.java
+++ TP4/src/yassir/wahid/exercice2/Produit.java
@@ -2,4 +2,14 @@ package yassir.wahid.exercice2;
 
 public class Produit {
 
+	int idProduit;
+
+	public int getIdProduit() {
+		return idProduit;
+	}
+
+	public void setIdProduit(int idProduit) {
+		this.idProduit = idProduit;
+	}
+
 }
