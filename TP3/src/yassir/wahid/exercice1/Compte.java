8b2c4e8947c6ebab266ab14761aa3fc181631e05
Y7T007 <yassir7t@gmail.com>
2023-12-18 11:05:57 +0100

    Update Eclipse metadata files

---
+++ TP3/src/yassir/wahid/exercice1/Compte.java
@@ -0,0 +1,42 @@
+package yassir.wahid.exercice1;
+
+
+public class Compte {
+	int numCompte;
+	int solde;
+	String proprietaire;
+
+	public int getNumCompte() {
+		return numCompte;
+	}
+	public String getProprietaire() {
+		return proprietaire;
+	}
+	public int getSolde() {
+		return solde;
+	}
+
+	public void setNumCompte(int numCompte) {
+		this.numCompte = numCompte;
+	}
+	public void setProprietaire(String proprietaire) {
+		this.proprietaire = proprietaire;
+	}
+	public void setSolde(int solde) {
+		this.solde = solde;
+	}
+
+	// constructor par default
+
+	public Compte() {
+
+	}
+
+	// Constructor par initialization
+
+	public Compte(int numCompte, int solde, String proprietaire) {
+		this.numCompte = numCompte;
+
+	}
+
+}
