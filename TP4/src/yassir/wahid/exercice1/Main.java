718b78d359f002ab67a96002781377d5b1244e90
Y7T007 <yassir7t@gmail.com>
2023-12-18 13:13:38 +0100

    Add user login functionality

--- TP4/src/yassir/wahid/exercice1/Main.java
+++ TP4/src/yassir/wahid/exercice1/Main.java
@@ -2,6 +2,7 @@ package yassir.wahid.exercice1;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
@@ -10,6 +11,30 @@ public class Main {
 		users.add(new Utilisateur(1, "yassir", 150000, "y7t007", "123456"));
 		users.add(new Utilisateur(2, "Wahid", 250000, "123@", "123456"));
 		users.add(new Utilisateur(3, "y7t007", 350000, "456@", "123456"));
+
+		int loggedIn = -1;
+		Scanner scanner = new Scanner(System.in);
+
+		do {
+			System.out.print("Insert your login infos : \n\n\t Email : ");
+			String email = scanner.next();
+			System.out.print("\t Password : ");
+			String password = scanner.next();
+
+			for (Utilisateur u : users) {
+				if (u.login.equals(email) && u.Password.equals(password)) {
+					System.out.print("\nLogged in successfully :) ");
+					loggedIn = u.id;
+					break;
+				}
+			}
+			if (loggedIn == -1) {
+				System.out.println("\nEchec d'authentification :( ");
+				continue;
+			}
+		} while (loggedIn == -1);
+
+
 	}
 
 }
