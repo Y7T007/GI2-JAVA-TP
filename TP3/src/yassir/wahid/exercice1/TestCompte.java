04bb41ae3342725b85ec09285365ddbbdbfc14f6
Y7T007 <yassir7t@gmail.com>
2023-12-18 11:30:07 +0100

    Update classpath and add test code for Compte class

--- TP3/src/yassir/wahid/exercice1/TestCompte.java
+++ TP3/src/yassir/wahid/exercice1/TestCompte.java
@@ -1,14 +1,28 @@
 package yassir.wahid.exercice1;
-
 import java.util.ArrayList;
+import java.util.List;
 
-public class Main {
-	
-	static ArrayList<Compte> Comptes = new ArrayList<>();
-	
-	static {
-		Comptes.add(new Compte(1, 1000, "John Doe"));
-		Comptes.add(new Compte(2, 2000, "Jane Smith"));
-		Comptes.add(new Compte(3, 3000, "Alice Johnson"));
+public class TestCompte {
+
+	public static void main(String[] args) {
+		ArrayList<Compte> comptes = new ArrayList<>();
+		comptes.add(new Compte(1, 1000, "John Doe"));
+		comptes.add(new Compte(2, 2000, "Jane Smith"));
+		comptes.add(new Compte(3, 3000, "Alice Johnson"));
+		
+		for (Compte compte : comptes) {
+			System.out.println(compte.toString());
+		}
+
+		List<Compte> comptos = new ArrayList<>();
+		comptos.add(new Compte(1, 85000, "John Doe"));
+		comptos.add(new Compte(2, 62000, "Wahid"));
+		comptos.add(new Compte(3, 873000, "Yassir"));
+
+		for (Compte compte : comptos) {
+			System.out.println(compte.toString());
+		}
 	}
+
 }
+
